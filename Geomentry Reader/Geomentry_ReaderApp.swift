8317cbce801a8e98d96eaf7f8a40a104a90bb3b5
abhinav2525 <56315478+abhinav2525@users.noreply.github.com>
2021-06-04 10:50:41 +0530

    Initial Commit

---
+++ Geomentry Reader/Geomentry_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Geomentry_ReaderApp.swift
+//  Geomentry Reader
+//
+//  Created by Abhinav krishna on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Geomentry_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
